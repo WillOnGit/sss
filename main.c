db55527aec081d0d62313acbb5e09ca8d30015a4
Will Bolton <git@bolton.me.uk>
2024-04-23 21:49:13 +0100

    Add boilerplate input handling code

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("placeholder\n");
+	FILE *infile;
+	char *secretfilename;
+	int c;
+
+	if (argc > 1 && strcmp(*(argv + 1), "-")) {
+		secretfilename = *(argv + 1);
+		infile = fopen(secretfilename, "r");
+
+		if (!infile) {
+			fprintf(stderr, "Unable to open file %s\n", secretfilename);
+			return 1;
+		}
+
+		fprintf(stderr, "Reading file %s\n", secretfilename);
+	} else {
+		fprintf(stderr, "Reading stdin\n");
+		infile = stdin;
+	}
+
+	while ((c = getc(infile)) != EOF)
+		putchar(c);
+
 	return 0;
 }
