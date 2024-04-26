0bd6bbfd86ee40d68339d5a7acad247564466098
Will Bolton <git@bolton.me.uk>
2024-04-26 21:55:37 +0100

    Add extremely basic encoding

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * from input 8-bit value, generate shares in the following scheme:
+ *
+ * k = 2 (implies polynomial degree 1)
+ * n = 3
+ *
+ * prime p = 257
+ * q(x) = secret + 57x
+ *
+ * yes, there are many deficiencies here, this is to get started.
+ */
+void shamir_encode(char inc)
+{
+	int y1, y2, y3, p;
+
+	p = 257;
+
+	y1 = (inc + 57 * 1) % p;
+	y2 = (inc + 57 * 2) % p;
+	y3 = (inc + 57 * 3) % p;
+
+	printf("(1, %d)\n(2, %d)\n(3, %d)\n", y1, y2, y3);
+}
+
+/*
+ * - read stdin or first argument filename
+ * - encode first character, if there is one
+ */
 int main(int argc, char **argv)
 {
 	FILE *infile;
@@ -22,8 +50,9 @@ int main(int argc, char **argv)
 		infile = stdin;
 	}
 
-	while ((c = getc(infile)) != EOF)
-		putchar(c);
+	c = getc(infile);
+	if (c != EOF)
+		shamir_encode(c);
 
 	return 0;
 }
